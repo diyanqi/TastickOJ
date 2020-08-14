b8fddaf6342cafa091d35a0f7d7f960aacc3a380
diyanqi <60802943+diyanqi@users.noreply.github.com>
2020-08-14 13:17:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -667,4 +667,4 @@ def judge_code():
     return jsonify(return_data)
 
 if __name__ == "__main__":
-	app.run(host = "0.0.0.0",port=8080,debug = True)
+	app.run(host = "0.0.0.0",port=8080,debug = False)
